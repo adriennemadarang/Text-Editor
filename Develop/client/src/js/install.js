3eeeeb6bd220c815017b89a1406db685fce35f16
adriennemadarang <adriennemadarang@icloud.com>
2023-10-03 19:32:20 -0700

    starter code and other things

---
+++ Develop/client/src/js/install.js
@@ -0,0 +1,31 @@
+const butInstall = document.getElementById('buttonInstall');
+
+// Logic for installing the PWA
+// TODO: Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+
+    window.defferedPrompt = event;
+//  Removes hidden class
+    butInstall.classList.toggle('hidden', false);
+});
+
+// TODO: Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.defferedPrompt;
+
+    if(!promptEvent) {
+        return;
+    }
+    // Shows prompt
+    promptEvent.prompt();
+
+    window.defferedPrompt = null;
+
+    butInstall.classList.toggle('hidden', true);
+});
+
+// TODO: Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+    // Clears prompt
+    window.defferedPrompt = null;
+});
